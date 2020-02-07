c41fbc5fb8009a1fa2744b5dcceb9e5c87288b05
Todd Greenberg <tsgreenberg1217@gmail.com>
2020-02-07 14:16:02 -0500

    refactor splashscreen

--- app/src/main/java/com/example/silent_film_trivia/activities/SplashScreen.kt
+++ app/src/main/java/com/example/silent_film_trivia/activities/SplashScreen.kt
@@ -1,6 +1,5 @@
 package com.example.silent_film_trivia.activities
 
-import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -9,19 +8,17 @@ import com.example.silent_film_trivia.SilentFilmTriviaApplication
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         goToGameIfInProgress()
     }
 
     private fun goToGameIfInProgress() {
         val id = SilentFilmTriviaApplication.prefsManager.getSessionId()
-        if (id > -1) {
-            val intent = Intent(this, TriviaActivity::class.java)
-            startActivity(intent)
-        } else {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        }
+        goToActivity(if (id > -1) TriviaActivity::class.java else HomeActivity::class.java)
         finish()
+    }
+
+    private fun <T> goToActivity(cls: Class<T>) {
+        val intent = Intent(this, cls)
+        startActivity(intent)
     }
 }
