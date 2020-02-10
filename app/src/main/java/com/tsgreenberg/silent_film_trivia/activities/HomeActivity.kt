b34640881fe68ec8ab42e7e66982364cd2ab152a
Todd Greenberg <tsgreenberg1217@gmail.com>
2020-02-10 14:40:41 -0500

    new title and refactor

--- app/src/main/java/com/tsgreenberg/silent_film_trivia/activities/HomeActivity.kt
+++ app/src/main/java/com/tsgreenberg/silent_film_trivia/activities/HomeActivity.kt
@@ -17,33 +17,33 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class HomeActivity : BaseActivity() {
-    lateinit var triviaIntent: Intent
     val titleAnimationHandler: Handler = Handler()
 
+    private lateinit var mTitleAimation: LottieAnimationView
+
     val titleAnimationRunnable: Runnable = Runnable {
-        mTitleAimation?.playAnimation()
+        mTitleAimation.playAnimation()
     }
-    var mTitleAimation: LottieAnimationView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        triviaIntent = Intent(this, TriviaActivity::class.java)
         mTitleAimation = createTitleAnimation()
         Btn_start.setOnClickListener { createAndStartTriviaSession() }
     }
 
 
     fun createAndStartTriviaSession() = lifecycleScope.launch(Dispatchers.IO) {
-//        val questions: ArrayList<Question> = SilentFilmTriviaApi.service.getQuestions()
+        //        val questions: ArrayList<Question> = SilentFilmTriviaApi.service.getQuestions()
         val questions = LocalQuestions.create()
         val session = Session(System.currentTimeMillis(), questions)
         val sessionId = SilentFilmTriviaApplication.database.sessionDao().insert(session)
         SilentFilmTriviaApplication.prefsManager.setSessionId(sessionId)
         launch(Dispatchers.Main) {
-            triviaIntent = Intent(this@HomeActivity, TriviaActivity::class.java)
-            triviaIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(triviaIntent)
+            Intent(this@HomeActivity, TriviaActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }.also { startActivity(it) }
             finish()
         }
     }
